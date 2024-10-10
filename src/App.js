3fc572605294c4dbc55c138e5cae8bb2b81ebbfc
IONHAX04 <thirukumara04@gmail.com>
2024-10-10 12:58:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Nav from "./Components/00-Navbar/Nav";
+import Dashboard from "./Components/01-Dashboard/Dashboard";
+
+
+// import { useNavigate } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Nav>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </Nav>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
